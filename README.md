e40e1d6b9d8f0de95ac27eec571c0be83e522bd0
ryuzinoh <whysafalishero@gmail.com>
2024-07-31 21:29:37 +0900

    update

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ This project provides a simple implementation of user authentication using Fireb
 
 ## issue
 so basically, i have a sensitive info that is very necessary to run this repo, but it's sensitive, so i can't add it, tho when i have added for short time, the test was successfull, yet i need to find a way to make the configs hidden yet the github pages work fluently, ciao!
+
+
+
+
+## closed !!, i will use certian framke work such as next for this project to make it use for field. cause pure js, html css, isnt a valid way to make this type of thing
--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,18 +3,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js"; 
 
 const firebaseConfig = {
-  apiKey: process.env.API,
-  databaseURL: process.env.DBURL,
-  authDomain: process.env.DOMAIN,
-  projectId: process.env.PID,
-  storageBucket: process.env.STOREBUCKET,
-  messagingSenderId: process.env.SENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASURED,
+  apiKey: "AIzaSyDwDYKCQhDRX27cT0DFC8y0VvSSvKBAG64",
+  authDomain: "sabers-72db7.firebaseapp.com",
+  databaseURL: "https://sabers-72db7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sabers-72db7",
+  storageBucket: "sabers-72db7.appspot.com",
+  messagingSenderId: "330705660301",
+  appId: "1:330705660301:web:abcff17179dd36442ff5c4",
+  measurementId: "G-CXHM5Y0JL6"  
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); 
 
 export { auth };
-
